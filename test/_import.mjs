e82e3c6e46109a8a7316f062a4616c40a72ec5c9
Antonio Laguna <sombragriselros@gmail.com>
2022-04-04 21:12:03 +0200

    Adding require and import tests
    #10

---
+++ test/_import.mjs
@@ -0,0 +1,4 @@
+import assert from 'assert';
+import plugin from 'markdown-it-image-figures';
+
+assert.equal(typeof plugin, 'function', 'should return a function');
---
+++ test/_require.cjs
@@ -0,0 +1,4 @@
+const assert = require('assert');
+const plugin = require('markdown-it-image-figures');
+
+assert.equal(typeof plugin, 'function', 'should return a function');
